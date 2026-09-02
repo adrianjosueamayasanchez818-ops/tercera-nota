03d5b9ee67bc0253a4cab6a9f1779cd5ea670ea9
adrian <adrianjosueamayasanchez818@gmail.com>
2026-09-02 18:57:50 -0500

    ejercicio de definir variables

---
+++ Nueva-carpeta/ejercicio3/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+  public static void main(String[] args) {
+   int edad = 18;
+double altura = 1.77;
+char inicialNombre = 'A';
+boolean mayorEdad = true;
+
+System.out.println("edad: " + edad);
+System.out.println("altura: " + altura);
+System.out.println("inicial de nombre: " + inicialNombre);
+System.out.println("es mayor de edad: " + mayorEdad);
+    }
+}
