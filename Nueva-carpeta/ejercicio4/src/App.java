87ce0bddaf2f8d1fa47e535ec528717342795de3
adrian <adrianjosueamayasanchez818@gmail.com>
2026-09-02 19:51:01 -0500

    ejercicio de variables constantes

---
+++ Nueva-carpeta/ejercicio4/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    final double PI = 3.1416;
+    double radio = 5;
+    double area = PI * radio * radio;
+    System.out.println("el area es de " + area);
+
+    }
+}
